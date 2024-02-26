21746882041c53daf39e9cae991cd7c16d12e22e
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-02-26 15:14:05 +0100

    prova

---
+++ src/main/java/it_epicode/buildweekFinale/BuildweekFinaleApplication.java
@@ -0,0 +1,13 @@
+package it_epicode.buildweekFinale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildweekFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildweekFinaleApplication.class, args);
+	}
+
+}
