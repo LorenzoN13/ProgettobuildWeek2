5a14c9afe2e5e07ef1573b566504962541943a5f
gabrieleiannuzzo <gabrieleiannuzzo10@gmail.com>
2024-02-27 11:51:01 +0100

    Aggiunta security chain

--- src/main/java/it_epicode/buildweekFinale/repository/ProvinciaRepository.java
+++ src/main/java/it_epicode/buildweekFinale/repository/ProvinciaRepository.java
@@ -2,10 +2,12 @@ package it_epicode.buildweekFinale.repository;
 
 import it_epicode.buildweekFinale.model.Provincia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ProvinciaRepository extends JpaRepository<Provincia, Integer> {
     public Optional<Provincia> getProvinciaByNome(String nome);
 
